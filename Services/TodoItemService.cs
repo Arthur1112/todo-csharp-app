b2256b5481b2f0a423d01b279790626813136606
Arthur <arthur_2335@hotmail.com>
2022-06-08 15:58:56 -0400

    added to todoitemservices

--- Services/TodoItemService.cs
+++ Services/TodoItemService.cs
@@ -1,4 +1,7 @@
+using Microsoft.Extensions.Options;
 using Models;
+using MongoDB.Driver;
+using Settings;
 
 namespace Services;
 
@@ -13,6 +16,14 @@ public interface ITodoItemService
 
 public class TodoItemService : ITodoItemService
 {
+  private readonly IMongoCollection<TodoItem> todoItemCollection;
+
+  public TodoItemService(IOptions<TodoDbSettings> todoDbSettings)
+  {
+    var client = new MongoClient(todoDbSettings.Value.ConnectionString);
+    var db = client.GetDatabase(todoDbSettings.Value.DatabaseName);
+    todoItemCollection = db.GetCollection<TodoItem>(todoDbSettings.Value.CollectionName);
+  }
   public Task<TodoItem> CreateTodoItem(TodoItem todoItem)
   {
     throw new NotImplementedException();
